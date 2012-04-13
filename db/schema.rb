c0d9c3ff3d5e517dd8141bd5e8a2f1c3ea697900
William Notowidagdo <wnotowidagdo@gmail.com>
2012-04-14 03:04:46 +0700

    Adding relationship between article and category

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120413194050) do
+ActiveRecord::Schema.define(:version => 20120413195310) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   create_table "categories", :force => true do |t|
